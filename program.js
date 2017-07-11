67e1475b35f40a204eb738adc476ca976fda030c
Vadim Yalovenko <yalovek@ya.ru>
2017-07-11 16:35:41 +0300

    Exercise 5

--- program.js
+++ program.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  console.log(data.split('\n').length - 1);
+const path = require('path');
+fs.readdir(process.argv[2], 'utf-8', (err, data) => {
+  data.forEach(name => {
+    if (path.extname(name) === `.${process.argv[3]}`) {
+      console.log(name);
+    }
+  });
 });
