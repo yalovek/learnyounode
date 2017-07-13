87b1ff4ecb2db42984c9182bbca35802e18e2968
Vadim Yalovenko <yalovek@ya.ru>
2017-07-13 12:20:58 +0300

    Exercise 11

--- program.js
+++ program.js
@@ -1,6 +1,8 @@
-const net = require('net')
-const zero = i => (i < 10 ? '0' : '') + i;
-const now = date => `${date.getFullYear()}-${zero(date.getMonth() + 1)}-${zero(date.getDate())} ${zero(date.getHours())}:${zero(date.getMinutes())}`;
-net.createServer(socket => {
-  socket.end(`${now(new Date())}\n`);
+const http = require('http');
+const fs = require('fs');
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'content-type': 'text/plain'
+  });
+  fs.createReadStream(process.argv[3]).pipe(res);
 }).listen(Number(process.argv[2]));
