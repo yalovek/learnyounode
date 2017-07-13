328138e8c7f7663e42943fe9fbde3d8f2fe65c11
Vadim Yalovenko <yalovek@ya.ru>
2017-07-13 12:30:28 +0300

    Exercise 12

--- program.js
+++ program.js
@@ -1,8 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const map = require('through2-map');
 http.createServer((req, res) => {
-  res.writeHead(200, {
-    'content-type': 'text/plain'
-  });
-  fs.createReadStream(process.argv[3]).pipe(res);
+  if (req.method === 'POST') {
+    req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res);
+  }
 }).listen(Number(process.argv[2]));
