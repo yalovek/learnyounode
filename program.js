f421a38e4aa39ce75806a4f92194096e8ad736af
Vadim Yalovenko <yalovek@ya.ru>
2017-07-13 12:48:52 +0300

    Exercise 13

--- program.js
+++ program.js
@@ -1,7 +1,23 @@
 const http = require('http');
-const map = require('through2-map');
+const url = require('url');
+const parseTime = time => ({
+  hour: time.getHours(),
+  minute: time.getMinutes(),
+  second: time.getSeconds()
+});
+const unixTime = time => ({
+  unixtime: time.getTime()
+});
+const getTime = url => /^\/api\/parsetime/.test(url)
+  ? parseTime
+  : /^\/api\/unixtime/.test(url)
+    ? unixTime
+    : () => null;
 http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res);
-  }
+  const parsedUrl = url.parse(req.url, true);
+  const time = new Date(parsedUrl.query.iso);
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(JSON.stringify(getTime(req.url)(time)));
 }).listen(Number(process.argv[2]));
